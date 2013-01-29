1ad56b0798750895ccdd5c908dd419d56aafb289
ivolapumapm87 <ivolapumaspam@gmail.com>
2013-01-29 20:51:51 -0200

    comentando atributo estabelecimento da classe EspetaculosController

--- src/main/java/br/com/caelum/agiletickets/controllers/EspetaculosController.java
+++ src/main/java/br/com/caelum/agiletickets/controllers/EspetaculosController.java
@@ -29,7 +29,7 @@ public class EspetaculosController {
 	private final Agenda agenda;
 	private Validator validator;
 	private Result result;
-	private Estabelecimento estabelecimento;
+	//private Estabelecimento estabelecimento;
 
 	private final DiretorioDeEstabelecimentos estabelecimentos;
 
